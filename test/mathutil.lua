7d34bcae5d31ecbd18ff59af7a2e330458ae970d
Robert Zenz <bobby@bonsaimind.org>
2014-12-13 16:40:55 +0100

    Added test for next_lower_prime.

--- test/mathutil.lua
+++ test/mathutil.lua
@@ -13,6 +13,22 @@ local function test_clamp()
 	test.equals(20, mathutil.clamp(30, 0, 20))
 end
 
+local function test_next_lower_prime()
+	test.equals(0, mathutil.next_lower_prime(0))
+	test.equals(1, mathutil.next_lower_prime(1))
+	test.equals(2, mathutil.next_lower_prime(2))
+	test.equals(3, mathutil.next_lower_prime(3))
+	test.equals(3, mathutil.next_lower_prime(4))
+	test.equals(5, mathutil.next_lower_prime(5))
+	test.equals(5, mathutil.next_lower_prime(6))
+	test.equals(7, mathutil.next_lower_prime(7))
+	test.equals(7, mathutil.next_lower_prime(8))
+	test.equals(7, mathutil.next_lower_prime(9))
+	
+	test.equals(619, mathutil.next_lower_prime(623))
+	test.equals(907, mathutil.next_lower_prime(910))
+end
+
 local function test_round()
 	test.equals(0, mathutil.round(0))
 	
@@ -29,5 +45,6 @@ end
 
 test.start("mathutil")
 test.run("clamp", test_clamp)
+test.run("next_lower_prime", test_next_lower_prime)
 test.run("round", test_round)
 
