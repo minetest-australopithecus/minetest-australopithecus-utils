9aaeabb7267f0038eb48c428de4b8c0d5841e386
Robert Zenz <bobby@bonsaimind.org>
2015-04-18 22:52:09 +0200

    Removed the shortcut again.

--- utils/tableutil.lua
+++ utils/tableutil.lua
@@ -287,7 +287,7 @@ function tableutil.to_string(table, indent)
 	
 	indent = indent or 0
 	
-	if type(table) == "table" and #table ~= 0 then
+	if type(table) == "table" then
 		local str = tostring(table) .. " {\n"
 		
 		local indentation = string.rep(" ", indent + 4)
