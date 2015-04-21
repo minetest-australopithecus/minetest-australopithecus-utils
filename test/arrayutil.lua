422b5e8312584c83ae82028dfd2943136a6359ad
Robert Zenz <Bobby@bonsaimind.org>
2015-04-21 21:04:07 +0200

    Added function for finding rows/column in arrays.

--- test/arrayutil.lua
+++ test/arrayutil.lua
@@ -11,6 +11,44 @@ dofile("./utils/tableutil.lua")
 test.start("arrayutil")
 
 
+test.run("next_matching_column", function()
+	local array = {
+		{ "", "", "", "", "" },
+		{ "", "something", "", "", "" },
+		{ "", "", "", "", "" },
+		{ "", "", "", "something", "" },
+		{ "", "", "", "", "" }
+	}
+	
+	test.equals(2, arrayutil.next_matching_column(array))
+	test.equals(4, arrayutil.next_matching_column(array, 3))
+	test.equals(-1, arrayutil.next_matching_column(array, 5))
+	
+	-- Reverse
+	test.equals(4, arrayutil.previous_matching_column(array))
+	test.equals(2, arrayutil.previous_matching_column(array, 3))
+	test.equals(-1, arrayutil.previous_matching_column(array, 1))
+end)
+
+test.run("next_matching_row", function()
+	local array = {
+		{ "", "", "", "", "" },
+		{ "", "", "something", "", "" },
+		{ "", "", "", "", "" },
+		{ "", "", "", "something", "" },
+		{ "", "", "", "", "" }
+	}
+	
+	test.equals(2, arrayutil.next_matching_row(array))
+	test.equals(4, arrayutil.next_matching_row(array, 3))
+	test.equals(-1, arrayutil.next_matching_row(array, 5))
+	
+	-- Reverse
+	test.equals(4, arrayutil.previous_matching_row(array))
+	test.equals(2, arrayutil.previous_matching_row(array, 3))
+	test.equals(-1, arrayutil.previous_matching_row(array, 1))
+end)
+
 test.run("reduce2d", function()
 	local reduced = arrayutil.reduce2d({})
 	local expected = {}
--- utils/arrayutil.lua
+++ utils/arrayutil.lua
@@ -33,6 +33,137 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 arrayutil = {}
 
 
+--- Finds the next matching column.
+--
+-- @param array The 2D array to search.
+-- @param start_index Optional. The index at which to start. Defaults to 1, or
+--                    if the direction is reversed, the number of columns in
+--                    the array.
+-- @param matcher Optional. The function that is used to determine if the column
+--                matches or not. Is expected to take one argument, the item,
+--                and return a boolean. The column matches if any of its items
+--                matches this condition. Defaults to not nil and not empty
+--                string.
+-- @param reverse Optional. If the array should be serched backwards. Defaults
+--                to false.
+-- @return The index of the matching column. -1 if none was found.
+function arrayutil.next_matching_column(array, start_index, matcher, reverse)
+	matcher = matcher or function(item)
+		return item ~= nil and item ~= ""
+	end
+	
+	local current_column = 0
+	
+	if reverse and start_index == nil then
+		for row_index = 1, #array, 1 do
+			local row = array[row_index]
+			
+			current_column = math.max(current_column, #row)
+		end
+	else
+		current_column = start_index or 1
+	end
+	
+	local had_column = true
+	
+	while had_column do
+		had_column = false
+		
+		for row_index = 1, #array, 1 do
+			local row = array[row_index]
+			
+			if current_column >= 1 and current_column <= #row then
+				had_column = true
+				
+				if matcher(row[current_column]) then
+					return current_column
+				end
+			end
+		end
+		
+		if reverse then
+			current_column = current_column - 1
+		else
+			current_column = current_column + 1
+		end
+	end
+	
+	return -1
+end
+
+--- Finds the next matching row.
+--
+-- @param array The 2D array to search.
+-- @param start_index Optional. The index at which to start. Defaults to 1, or
+--                    if the direction is reversed, the number of rows in
+--                    the array.
+-- @param matcher Optional. The function that is used to determine if the row
+--                matches or not. Is expected to take one argument, the item,
+--                and return a boolean. The row matches if any of its items
+--                matches this condition. Defaults to not nil and not empty
+--                string.
+-- @param reverse Optional. If the array should be serched backwards. Defaults
+--                to false.
+-- @return The index of the matching row. -1 if none was found.
+function arrayutil.next_matching_row(array, start_index, matcher, reverse)
+	matcher = matcher or function(item)
+		return item ~= nil and item ~= ""
+	end
+	
+	local to = #array
+	local step = 1
+	
+	if reverse then
+		start_index = start_index or #array
+		to = 1
+		step = -1
+	else
+		start_index = start_index or 1
+	end
+	
+	for row_index = start_index, to, step do
+		local row = array[row_index]
+		
+		for column_index = 1, #row, 1 do
+			if matcher(row[column_index]) then
+				return row_index
+			end
+		end
+	end
+	
+	return -1
+end
+
+--- Finds the previous matching column.
+--
+-- @param array The 2D array to search.
+-- @param start_index Optional. The index at which to start. Defaults to
+--                    the number columns in the array.
+-- @param matcher Optional. The function that is used to determine if the column
+--                matches or not. Is expected to take one argument, the item,
+--                and return a boolean. The column matches if any of its items
+--                matches this condition. Defaults to not nil and not empty
+--                string.
+-- @return The index of the matching column. -1 if none was found.
+function arrayutil.previous_matching_column(array, start_index, matcher)
+	return arrayutil.next_matching_column(array, start_index, matcher, true)
+end
+
+--- Finds the previous matching row.
+--
+-- @param array The 2D array to search.
+-- @param start_index Optional. The index at which to start. Defaults to
+--                    the number rows in the array.
+-- @param matcher Optional. The function that is used to determine if the row
+--                matches or not. Is expected to take one argument, the item,
+--                and return a boolean. The row matches if any of its items
+--                matches this condition. Defaults to not nil and not empty
+--                string.
+-- @return The index of the matching row. -1 if none was found.
+function arrayutil.previous_matching_row(array, start_index, matcher)
+	return arrayutil.next_matching_row(array, start_index, matcher, true)
+end
+
 --- Removes empty rows and columns at the beginning and the end of the given
 -- array.
 --
@@ -42,54 +173,11 @@ arrayutil = {}
 --                 empty. Expected is a function that takes one item and returns
 --                 a boolean.
 function arrayutil.reduce2d(array, is_empty)
-	is_empty = is_empty or function(item)
-		return item == nil or item == ""
-	end
+	local first_row = arrayutil.next_matching_row(array, nil, is_empty)
+	local last_row = arrayutil.previous_matching_row(array, nil, is_empty)
 	
-	local first_row = -1
-	local last_row = -1
-	
-	for row_index = 1, #array, 1 do
-		local row = array[row_index]
-		local row_is_empty = true
-		
-		for column_index = 1, #row, 1 do
-			if not is_empty(row[column_index]) then
-				row_is_empty = false
-			end
-		end
-
-		if not row_is_empty then
-			if first_row == -1 then
-				first_row = row_index
-			else
-				last_row = row_index
-			end
-		end
-	end
-	
-	local first_column = -1
-	local last_column = -1
-	
-	for column_index = 1, 5, 1 do
-		local column_is_empty = true
-		
-		for row_index = 1, #array, 1 do
-			local row = array[row_index]
-			
-			if column_index <= #row and not is_empty(row[column_index]) then
-				column_is_empty = false
-			end
-		end
-		
-		if not column_is_empty then
-			if first_column == -1 then
-				first_column = column_index
-			else
-				last_column = column_index
-			end
-		end
-	end
+	local first_column = arrayutil.next_matching_column(array, nil, is_empty)
+	local last_column = arrayutil.previous_matching_column(array, nil, is_empty)
 	
 	if last_row == -1 then
 		last_row = first_row
